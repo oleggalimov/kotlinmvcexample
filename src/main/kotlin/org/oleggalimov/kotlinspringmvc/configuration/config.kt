efef6e75da2be1d401ebbc16f218c57aad30cebc
oleg <o.d.galimov@yandex.ru>
2018-07-20 21:33:14 +0500

    initial commit

--- src/main/kotlin/org/oleggalimov/kotlinspringmvc/configuration/config.kt
+++ src/main/kotlin/org/oleggalimov/kotlinspringmvc/configuration/config.kt
@@ -1,7 +1,22 @@
 package org.oleggalimov.kotlinspringmvc.configuration
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.view.InternalResourceViewResolver
+import org.springframework.web.servlet.ViewResolver
+import org.springframework.context.annotation.Bean
+
+
 
 
 @Configuration
-class config
+class config {
+    @Bean
+    fun viewResolver(): ViewResolver {
+        val viewResolver = InternalResourceViewResolver()
+        //        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/pages/")
+        viewResolver.setSuffix(".jsp")
+
+        return viewResolver
+    }
+}
